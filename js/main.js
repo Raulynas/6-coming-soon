f098a7f061b3544f84b4a739eda0a2c28cdd355e
raulynas <laurynas@inwoodengineering.co.uk>
2021-01-23 20:23:21 +0100

    Render socials #20

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-console.log("coming soon");
+// call function
+import { renderSocials } from "./components/renderSocials.js";
+// take data
+import { socialsData } from "./data/socialsData.js";
+// push data to funciton
+
+renderSocials(socialsData);
