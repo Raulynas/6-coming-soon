b4e93931d0eaef87dd6e1470cd3e5cc7e95c20f2
raulynas <laurynas@inwoodengineering.co.uk>
2021-01-27 20:22:43 +0100

    Progress bars and CSS variables #19 #22

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 import { renderSocials } from "./components/renderSocials.js";
 import { socialsData } from "./data/socialsData.js";
 
+import { renderProgressBars } from "./components/renderProgressBars.js";
+import { progressBarData } from "./data/progressBarData.js";
+
+renderProgressBars(".left-column", progressBarData);
 renderSocials("footer > .socials", socialsData);
