242180eb33cdab4146a555eb4cf4509ddcb3535b
raulynas <laurynas@inwoodengineering.co.uk>
2021-02-01 18:21:49 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,12 +6,3 @@ import { progressBarData } from "./data/progressBarData.js";
 
 renderProgressBars(".left-column", progressBarData);
 renderSocials("footer > .socials", socialsData);
-
-// renderProgressBars(".left-column", [{ ssdf }]);
-// renderProgressBars(".left-column", progressBarData);
-
-// renderProgressBars(".left-column", [
-//   { label: "Testing", value: 50 },
-//   { label: "Testing", value: 50.4 },
-//   { label: "Testing", value: 50.5431 },
-// ]);
