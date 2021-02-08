1d5dbcbb781ca4fd30af395f6e4b098fa41dfa54
raulynas <laurynas@inwoodengineering.co.uk>
2021-02-08 10:50:36 +0100

    Validator comments

--- js/components/form/Validator.js
+++ js/components/form/Validator.js
@@ -5,9 +5,13 @@ class Validator {
         } else {
             return true;
         }
+        //tik vienas zodis
+        //tik abeceles raides
+        //pirma raide didzioji, kitos mazosios (slice, upper case and lower case methods)
     }
 
     isValidEmail(email) {
+        // read more: email adderss is wikipedia
         if (typeof email !== "string" || email === "" || email.trim() === "") {
             return "Email negali buti tuscias tekstas";
         } else {
@@ -21,6 +25,8 @@ class Validator {
         } else {
             return true;
         }
+        // tuti tureti @ raide
+        // negali buti tarpu pries ir po @
     }
 }
 export { Validator };
